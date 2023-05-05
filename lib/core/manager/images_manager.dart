7b19f051a7e77dd51bac1bba47dcda22e825694d
Amr Hassan <amrkhlifa4@gmail.com>
2023-05-05 17:25:52 +0200

    remove unused images

--- lib/core/manager/images_manager.dart
+++ lib/core/manager/images_manager.dart
@@ -1,9 +1,5 @@
 const String _imagesPath = 'assets/images';
 
 class ImagesManager {
-  static const String emergency = '$_imagesPath/emergency.jpg';
-  static const String carOil = '$_imagesPath/car_oil.jpg';
-  static const String questions = '$_imagesPath/q.jpg';
-  static const String mechanic = '$_imagesPath/mechanic.jpg';
-  static const String mechanic2 = '$_imagesPath/mm2.jpg';
+
 }
