b397d048396be96101870d28982f71bde9e49a87
Amr Hassan <amrkhlifa4@gmail.com>
2023-05-06 11:04:20 +0200

    move theme to core/theme.dart

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:mechanic/core/manager/colors_manager.dart';
+
+ThemeData appTheme(BuildContext context){
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    textButtonTheme: TextButtonThemeData(
+      style: ButtonStyle(
+        textStyle: MaterialStateProperty.all(
+          const TextStyle(
+            color: ColorsManager.primary,
+          ),
+        ),
+      ),
+    ),
+    appBarTheme: const AppBarTheme(
+      iconTheme: IconThemeData(color: Colors.grey),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        color: ColorsManager.primary,
+        fontWeight: FontWeight.bold,
+        fontSize: 18,
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mechanic/core/bindings.dart';
-import 'package:mechanic/core/manager/colors_manager.dart';
+import 'package:mechanic/core/theme/theme.dart';
 import 'package:mechanic/data/data_sources/hive_helper.dart';
 import 'package:mechanic/features/auth/view/login_screen.dart';
 import 'package:mechanic/features/home/home_screen.dart';
@@ -27,20 +27,7 @@ class MyApp extends StatelessWidget {
       initialBinding: AppBinding(),
       textDirection: TextDirection.rtl,
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          iconTheme: IconThemeData(color: Colors.grey),
-          backgroundColor: Colors.white,
-          elevation: 0,
-          centerTitle: true,
-          titleTextStyle: TextStyle(
-            color: ColorsManager.primary,
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-          ),
-        ),
-      ),
+      theme: appTheme(context),
       home: controlView(),
     );
   }
