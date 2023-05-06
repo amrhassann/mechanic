583444fcaed650908ae7346688f8c2635d65a165
Amr Hassan <amrkhlifa4@gmail.com>
2023-05-06 13:08:22 +0200

    add car coming soon

---
+++ lib/features/cars/view/create_car_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CreateCar extends StatelessWidget {
+  const CreateCar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: const Center(child: Text('Coming Soon')));
+  }
+}
