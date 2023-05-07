1d14a3f9413ca9a562be9440a2f1d9dc9b360043
Amr Hassan <amrkhlifa4@gmail.com>
2023-05-07 15:08:59 +0200

    add GetCarsController to AppBindings

--- lib/core/bindings.dart
+++ lib/core/bindings.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:mechanic/features/auth/controller/create_account_controller.dart';
 import 'package:mechanic/features/auth/controller/login_controller.dart';
 import 'package:mechanic/features/car_status/controller/car_status_controller.dart';
+import 'package:mechanic/features/cars/controller/get_cars_controller.dart';
 import 'package:mechanic/features/q_and_a/controller/controller.dart';
 
 class AppBinding extends Bindings {
@@ -12,5 +13,6 @@ class AppBinding extends Bindings {
     Get.lazyPut(() => CreateAccountController());
     Get.lazyPut(() => LoginController());
     Get.lazyPut(() => QuestionsController());
+    Get.lazyPut(() => GetCarsController());
   }
 }
