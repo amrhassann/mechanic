4a4d642b55e7c412313fdde8cd2c204fa280ecf5
Amr Hassan <amrkhlifa4@gmail.com>
2023-05-07 15:07:57 +0200

    get cars from firestore

---
+++ lib/features/cars/controller/get_cars_controller.dart
@@ -0,0 +1,42 @@
+import 'package:get/get.dart';
+import 'package:mechanic/core/enums.dart';
+import 'package:mechanic/core/functions.dart';
+import 'package:mechanic/data/data_sources/car_data_source.dart';
+import 'package:mechanic/data/models/car_model/car_model.dart';
+
+class GetCarsController extends GetxController{
+
+  final BaseCareDataSource _source = CarDataSource();
+  bool loading= true;
+
+  changeLoadingValue(){
+    loading = !loading;
+    update();
+  }
+
+
+  List<CarModel> cars = [];
+
+
+  Future<void> getCars() async{
+    final result = await _source.getUserCars();
+    if(result.requestState == RequestState.success){
+      changeLoadingValue();
+      cars = result.data!;
+    }else{
+      changeLoadingValue();
+      Get.back();
+      snackBarError(result.errorMessage??"Error");
+    }
+  }
+
+
+
+  @override
+  void onInit() {
+    getCars();
+    super.onInit();
+  }
+
+
+}
